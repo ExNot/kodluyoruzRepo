2155cd96d81cd507007631d84e7b8df0336fc5f5
Enes Baskale <enesbaskale@gmail.com>
2022-12-17 16:09:33 +0300

    Kdv

---
+++ src/main/java/org/example/Kdv.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Kdv {
+    public static void main(String[] args) {
+        int math,physics,chemistry,english,history,music;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Your Math Degree: ");
+        math = scan.nextInt();
+
+        System.out.println("Your Physics Grade: ");
+        physics = scan.nextInt();
+
+        System.out.println("Your Chemistry Grade: ");
+        chemistry = scan.nextInt();
+
+        System.out.println("Your English Grade: ");
+        english = scan.nextInt();
+
+        System.out.println("Your History Grade: ");
+        history = scan.nextInt();
+
+        System.out.println("Your Music Grade: ");
+        music = scan.nextInt();
+
+        int sum = math+physics+chemistry+english+history+music;
+        double avg = sum/6;
+        System.out.println("Your avg: "+avg);
+
+        String passControl = avg>=50 ? "Passed" : "Failed";
+        System.out.println(passControl);
+    }
+}
