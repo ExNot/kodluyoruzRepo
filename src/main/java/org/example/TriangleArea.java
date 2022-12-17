65ffddcaf15af9f7b32e84dc85521866790a5e42
Enes Baskale <enesbaskale@gmail.com>
2022-12-18 00:15:39 +0300

    Triangle Area

---
+++ src/main/java/org/example/TriangleArea.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter first edge: ");
+        int e1 = scan.nextInt();
+        System.out.println("Please enter second edge: ");
+        int e2 = scan.nextInt();
+        System.out.println("Please enter third edge: ");
+        int e3 = scan.nextInt();
+        double u = (e1+e2+e3)/2.0f;
+        double area = Math.sqrt(u*(u-e1)*(u-e2)*(u-e3));
+        System.out.println("The area of triengle is:" + area);
+    }
+}
