b078faf701c6b34b7fa22853460a81866650757a
Enes Baskale <enesbaskale@gmail.com>
2022-12-23 10:26:00 +0300

    FibonacciWithRecrusiveMethod

---
+++ src/main/java/Method/FibonacciWithRecursive.java
@@ -0,0 +1,15 @@
+package Method;
+
+public class FibonacciWithRecursive {
+
+    static int fibonacci(int n){
+
+        if (n == 1 || n == 2) return 1;
+        return fibonacci(n-1) + fibonacci(n-2);
+
+    }
+    public static void main(String[] args) {
+        System.out.println(fibonacci(27));
+    }
+
+}
